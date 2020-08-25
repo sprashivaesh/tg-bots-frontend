1fc6398068a18cbd29c72b49045aaf4674a81368
Roman Prishchepa <sprashivaesh@yandex.ru>
2020-08-25 12:00:21 +0300

    add inProgress in autoAnswer

--- src/components/Forms/AutoAnswerForm.tsx
+++ src/components/Forms/AutoAnswerForm.tsx
@@ -7,7 +7,7 @@ import classNames from 'classnames'
 
 type Props = {
   values: FormValues
-  onSubmit: (id:number, values: FormValues) => void
+  onSubmit: (id: number, values: FormValues) => void
   id: number
   isSaving: boolean
   onDelete?: (id: number) => void
@@ -29,41 +29,50 @@ const AutoAnswerForm: FC<Props> = ({values, id, onSubmit, onDelete, isSaving, is
     validationSchema: schema,
     onSubmit: (values) => onSubmit(id, values)
   })
+
+  const inProgress = isSaving || !!isDeleting
+
   // console.log(id)
   // console.log(form.dirty)
 
   return (
     <form onSubmit={form.handleSubmit} className="mb-5 col-10 mx-auto">
       <div className="row text-left">
-        <div className={classNames('col-12 col-lg-5 form-group label-floating', {'has-danger': (form.touched.coincidences && form.errors.coincidences)})}>
-          <label className="form-control-label bmd-label-floating" htmlFor={'coincidencesTextarea'+id}>
+        <div
+          className={classNames('col-12 col-lg-5 form-group label-floating', {'has-danger': (form.touched.coincidences && form.errors.coincidences)})}>
+          <label className="form-control-label bmd-label-floating" htmlFor={'coincidencesTextarea' + id}>
             Фразы, которые бот увидит в тексте, каждая с новой строки
           </label>
           <textarea
             className="form-control"
             rows={5}
             name="coincidences"
-            id={'coincidencesTextarea'+id}
+            id={'coincidencesTextarea' + id}
             value={form.values.coincidences}
             onChange={form.handleChange}
             onBlur={form.handleBlur}
+            disabled={inProgress}
           />
-          {form.touched.coincidences && form.errors.coincidences && <span className="bmd-label-floating">{form.errors.coincidences}</span>}
+          {form.touched.coincidences && form.errors.coincidences &&
+          <span className="bmd-label-floating">{form.errors.coincidences}</span>}
         </div>
-        <div className={classNames('col-12 col-lg-5 form-group label-floating', {'has-danger': (form.touched.answers && form.errors.answers)})}>
-          <label className="form-control-label bmd-label-floating" htmlFor={'answersTextarea'+id}>
+        <div
+          className={classNames('col-12 col-lg-5 form-group label-floating', {'has-danger': (form.touched.answers && form.errors.answers)})}>
+          <label className="form-control-label bmd-label-floating" htmlFor={'answersTextarea' + id}>
             Фразы, которые бот будет отвечать, каждая с новой строки, для фраз с переносом строк используйте <b>\n</b>
           </label>
           <textarea
             className="form-control"
             rows={5}
             name="answers"
-            id={'answersTextarea'+id}
+            id={'answersTextarea' + id}
             onChange={form.handleChange}
             onBlur={form.handleBlur}
             value={form.values.answers}
+            disabled={inProgress}
           />
-          {form.touched.answers && form.errors.answers && <span className="bmd-label-floating">{form.errors.answers}</span>}
+          {form.touched.answers && form.errors.answers &&
+          <span className="bmd-label-floating">{form.errors.answers}</span>}
         </div>
         <div className="col-12 col-lg-2">
           <div className="form-check">
@@ -74,21 +83,26 @@ const AutoAnswerForm: FC<Props> = ({values, id, onSubmit, onDelete, isSaving, is
                 name="private"
                 onChange={form.handleChange}
                 checked={form.values.private}
+                disabled={inProgress}
               />
               Приватно
               <span className="form-check-sign"><span className="check"/></span>
             </label>
           </div>
-          {onDelete?(
-            <button className="btn btn-danger btn-sm d-block w-100" type="button" onClick={() => onDelete(id)} disabled={isDeleting}>
-              Удалить
-              {isDeleting?<span className="spinner"/>:''}
-            </button>)
-            :''}
-          <button className="btn btn-sm d-block w-100" type="button" onClick={() => form.resetForm()} disabled={!form.dirty}>Восстановить</button>
-          <button className="btn btn-success btn-sm d-block w-100" type="submit" disabled={!form.dirty || isSaving}>
+          {onDelete ? (
+              <button className="btn btn-danger btn-sm d-block w-100" type="button" onClick={() => onDelete(id)}
+                      disabled={inProgress}>
+                Удалить
+                {isDeleting ? <span className="spinner"/> : ''}
+              </button>)
+            : ''}
+          <button className="btn btn-sm d-block w-100" type="button" onClick={() => form.resetForm()}
+                  disabled={!form.dirty || inProgress}>Восстановить
+          </button>
+          <button className="btn btn-success btn-sm d-block w-100" type="submit"
+                  disabled={!form.dirty || inProgress}>
             Сохранить
-            {isSaving?<span className="spinner"/>:''}
+            {isSaving ? <span className="spinner"/> : ''}
           </button>
         </div>
       </div>
