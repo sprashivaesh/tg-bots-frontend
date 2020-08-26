d27b51f91d5470c2b33c5169beec083375411044
Roman Prishchepa <sprashivaesh@yandex.ru>
2020-08-26 12:40:43 +0300

    change page notFound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -8,8 +8,8 @@ const NotFound = () => {
         <div className="row">
           <div className="col-md-12">
             <h1 className="title">404</h1>
-            <h2 className="description">Page not found :(</h2>
-            <h4 className="description">Ooooups! Looks like you got lost.</h4>
+            <h2 className="description">Страница не найдена :(</h2>
+            <h4 className="description">Упс! Похоже, ты заблудился.</h4>
           </div>
         </div>
       </div>
